a648613322149d59fa13c5a12bd71c87687fe8b0
Milan Swanthra <wkmswanthra@gmail.com>
2024-01-21 12:37:41 +0530

    Complete response routes

--- app/api/students/route.js
+++ app/api/students/route.js
@@ -1,43 +1,73 @@
 import { NextRequest, NextResponse } from "next/server";
 import {
   getAllStudents,
+  getStudentById,
   createStudent,
   updateStudentById,
   deleteStudentById,
 } from "../../../controllers/studentController";
 
+const handleSuccessResponse = (data) => {
+  return NextResponse.json({
+    status: "success",
+    data: data,
+  });
+};
+
+const handleErrorResponse = (error) => {
+  return NextResponse.json({
+    status: "error",
+    error: {
+      message: error.message,
+    },
+  });
+};
+
 export const GET = async (req, res) => {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
   try {
-    const students = await getAllStudents();
-    return NextResponse.json(students);
+    if (id) {
+      const student = await getStudentById(id);
+      return handleSuccessResponse(student);
+    } else {
+      const students = await getAllStudents();
+      return handleSuccessResponse(students);
+    }
   } catch (err) {
-    return NextResponse.json(err);
+    return handleErrorResponse(err);
   }
 };
 
 export const POST = async (req, res) => {
+  const body = await req.json();
   try {
-    const student = await createStudent(req.body);
-    return NextResponse.json(student);
+    const student = await createStudent(body);
+    return handleSuccessResponse(student);
   } catch (err) {
-    return NextResponse.json(err);
+    return handleErrorResponse(err);
   }
 };
 
 export const PUT = async (req, res) => {
+  const body = await req.json();
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
   try {
-    const student = await updateStudentById(req.body);
-    return NextResponse.json(student);
+    const student = await updateStudentById(id, body);
+    return handleSuccessResponse(student);
   } catch (err) {
-    return NextResponse.json(err);
+    return handleErrorResponse(err);
   }
 };
 
 export const DELETE = async (req, res) => {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
   try {
-    const student = await deleteStudentById(req.body);
-    return NextResponse.json(student);
+    const student = await deleteStudentById(id);
+    return handleSuccessResponse(student);
   } catch (err) {
-    return NextResponse.json(err);
+    return handleErrorResponse(err);
   }
 };
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -7,17 +7,80 @@ const getAllStudents = async () => {
     const students = await prisma.student.findMany();
     return students;
   } catch (err) {
-    return err;
+    console.error("Error fetching all students:", err);
+    throw new Error("Error fetching all students");
   }
 };
 
-const getStudentById = async (req, res) => {};
+const getStudentById = async (id) => {
+  console.log(id);
+  try {
+    const student = await prisma.student.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    return student;
+  } catch (err) {
+    console.error("Error fetching student by id:", err);
+    throw new Error("Error fetching student by id");
+  }
+};
 
-const createStudent = async (req, res) => {};
+const createStudent = async (req) => {
+  try {
+    const student = await prisma.student.create({
+      data: {
+        firstName: req.firstName,
+        lastName: req.lastName,
+        dateOfBirth: req.dateOfBirth,
+        email: req.email,
+        phone: req.phone,
+        address: req.address,
+      },
+    });
+    return student;
+  } catch (err) {
+    console.error("Error creating student:", err);
+    throw new Error("Error creating student");
+  }
+};
 
-const updateStudentById = async (req, res) => {};
+const updateStudentById = async (id, body) => {
+  try {
+    const student = await prisma.student.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        firstName: body.firstName,
+        lastName: body.lastName,
+        dateOfBirth: body.dateOfBirth,
+        email: body.email,
+        phone: body.phone,
+        address: body.address,
+      },
+    });
+    return student;
+  } catch (err) {
+    console.error("Error updating student:", err);
+    throw new Error("Error updating student");
+  }
+};
 
-const deleteStudentById = async (req, res) => {};
+const deleteStudentById = async (id) => {
+  try {
+    const student = await prisma.student.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    return student;
+  } catch (err) {
+    console.error("Error deleting student:", err);
+    throw new Error("Error deleting student");
+  }
+};
 
 module.exports = {
   getAllStudents,
